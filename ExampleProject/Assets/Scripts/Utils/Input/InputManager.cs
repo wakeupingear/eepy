b871f99314e305a88a83728b8db81b4fafe3fbf2
Abby Farhat <abbysfarhat@gmail.com>
2025-07-23 18:57:57 -0700

    fix: added second DISABLESTEAMWORKS to InputManager

--- ExampleProject/Assets/Scripts/Utils/Input/InputManager.cs
+++ ExampleProject/Assets/Scripts/Utils/Input/InputManager.cs
@@ -1,3 +1,7 @@
+#if !(UNITY_STANDALONE_WIN || UNITY_STANDALONE_LINUX || UNITY_STANDALONE_OSX || STEAMWORKS_WIN || STEAMWORKS_LIN_OSX) || UNITY_WEBGL
+#define DISABLESTEAMWORKS
+#endif
+
 using System;
 using System.Collections.Generic;
 using UnityEngine;
